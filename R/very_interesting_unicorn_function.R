9c66a6d9cc8e159201d922957f335e1e649531ce
MEK <m.eliaszkowalska@gmail.com>
2022-07-06 20:01:36 +0200

    commit

---
+++ R/very_interesting_unicorn_function.R
@@ -0,0 +1,10 @@
+#' Title: very_interesting_unicorn_function
+#'
+#' @return
+#' @export
+#'
+#' @examples
+very_interesting_unicorn_function <- function(){
+  x<- "I guess not so interesting"
+  print(x)
+}
